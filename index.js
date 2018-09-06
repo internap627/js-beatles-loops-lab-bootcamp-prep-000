4deee674ec8003592a5747d40d2d7eca74ff27ca
Nkosi Phillip <internap627@gmail.com>
2018-09-06 02:17:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(mus, ins){
+  
+  var arr = [];
+  for(var i = 0 ; i < mus.length ; i++){
+    arr.push(`${mus[i] plays ${ins[i]}`);
+  }
+  return arr;
+}
